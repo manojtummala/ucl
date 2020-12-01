228c205cf9fb3f4b3c7d7f322fabc3986df9e4b5
manojtummala <tummmalamanoj2002@gmail.com>
2020-12-01 21:07:08 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'sup-sathyabama',
-    version = '0.1',
+    name = 'ucl_sathya',
+    version = '0.2',
     author = 'Manoj Tummala',
     author_email = 'tummalamanoj2002@gmail.com',
     description = 'Fetches latest updates of news and events and Pretty prints it on terminal.',
     url='https://github.com/manojtummala/SUP',
-    py_modules = ['sup'],
     install_requires=[
         'Click', 'requests', 'tabulate'
+    ],
+    py_modules=['ucl'],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     entry_points='''
         [console_scripts]
